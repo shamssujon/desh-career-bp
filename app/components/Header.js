08e60ca7a118ef5673bb9a3e2817e0a366d2973a
Shamsul Islam Sujon <sujon2762@gmail.com>
2023-06-10 22:44:07 +0600

    Header responsive menu

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import Logo from "../assets/img/logo.png";
 
 const navLinks = [
@@ -15,27 +16,54 @@ const navLinks = [
 ];
 
 export default function Header() {
+	const [openMenu, handleOpenMenu] = useState(false);
 	const pathname = usePathname();
 
+	const handleMenuToggle = () => {
+		handleOpenMenu(!openMenu);
+		console.log(openMenu);
+	};
+
 	return (
-		<div className="bg-white shadow py-4">
-			<div className="container flex items-center">
-				<Link href="/">
-					<Image src={Logo} width="150" height="150" alt="Logo" />
+		<div className="relative bg-white py-4 shadow-md">
+			<div className="container flex flex-wrap items-center justify-between">
+				<Link href="/" className="shrink-0">
+					<Image src={Logo} width="180" height="150" alt="Logo" className="h-auto w-36 lg:w-44" />
 				</Link>
-				<div className="flex items-center ml-auto gap-2">
-					<ul className="flex items-center gap-2">
+				<button
+					onClick={handleMenuToggle}
+					className={`relative flex h-6 w-10 flex-col items-center justify-center before:absolute before:inset-x-0 before:h-0.5 before:w-full before:origin-center before:transform before:bg-black before:transition-all before:content-[''] after:absolute after:inset-x-0 after:h-0.5 after:w-full after:origin-center after:transform after:bg-black after:transition-all after:content-[''] lg:hidden ${
+						openMenu
+							? "before:-translate-y-1/2 before:-rotate-45 after:-translate-y-1/2 after:rotate-45"
+							: "before:-translate-y-3 before:rotate-0 after:translate-y-3 after:rotate-0"
+					}`}>
+					<span className="sr-only">Menu</span>
+					<span
+						className={`absolute inset-x-0 block h-0.5 bg-black transition-all ${
+							openMenu ? "w-0" : "w-full"
+						}`}></span>
+				</button>
+				<div
+					className={`absolute inset-x-0 top-full flex w-full flex-1 origin-top transform flex-col items-center gap-2 bg-white p-4 shadow-md transition-all lg:static lg:flex-row lg:justify-end lg:bg-transparent lg:p-0 lg:shadow-none ${
+						openMenu ? "scale-y-100" : "scale-y-0 lg:scale-y-100"
+					}`}>
+					<ul className="flex flex-col items-center gap-2 lg:flex-row">
 						{navLinks.map(({ name, url }) => {
 							return (
 								<li key={name}>
-									<Link href={url} className={`${pathname === url ? "text-blue-500" : ""} font-bold`}>
+									<Link
+										onClick={handleMenuToggle}
+										href={url}
+										className={`${
+											pathname === url ? "text-rose-600" : "text-black"
+										} p-2 font-semibold transition hover:text-rose-600`}>
 										{name}
 									</Link>
 								</li>
 							);
 						})}
 					</ul>
-					<Link href={"/"} className="bg-red-600 text-white px-4 py-2 rounded">
+					<Link href={"/"} className="rounded bg-rose-600 px-4 py-2 text-white transition hover:bg-rose-700">
 						Join Now
 					</Link>
 				</div>
